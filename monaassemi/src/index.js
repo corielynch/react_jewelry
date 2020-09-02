0408cf1015ade13ffc1eccc3833c8ef00a8ae102
Batman <bgmyhan@gmail.com>
2020-09-02 11:07:00 -0700

    Deleted less files in the original cli application download. Created only one file inside src index.js to create CRUD for testing purposes.

--- monaassemi/src/index.js
+++ monaassemi/src/index.js
@@ -1,17 +1,15 @@
+// Import React and React Dom
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
 
+
+// Create React Component
+const App = () => {
+return <div>Mona Assemi</div>;
+};
+
+// Take the react component and print to screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    <App />,
+    document.querySelector('#root')
+)
