c2377cdeb67d77b6ff53cf137f2973363e67dbc2
Batman <bgmyhan@gmail.com>
2020-09-02 17:06:55 -0700

    Created basic routes for our shop, product, and order pages so we can easily build them out once we have the content from the original site and update them with the clients business information

--- monaassemi/src/components/App.js
+++ monaassemi/src/components/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
-
+import{BrowserRouter, Route, Link} from 'react-router-dom';
+import OrderPage from './pages/Order-Page';
+import ProductPage from './pages/Product-Page';
+import ShopPage from './pages/Shop-Page';
 
 const App =  () => {
-    return <h1>Mona Assemi</h1>
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path='/' exact component={ShopPage} />
+                    <Route path='/product-page' exact component={ProductPage}></Route>
+                    <Route path='/order-page' exact component={OrderPage}></Route>
+                </div>
+            </BrowserRouter>
+        </div>
+        )
 }
 
 export default App;
