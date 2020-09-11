614678492dcd19e5bf146f8f9f5091f5af1e22a4
Corie Lynch <61636614+corielynch@users.noreply.github.com>
2020-09-10 21:12:09 -0700

    Revert "Product Card Component in header.js  now links to products page via b…"

--- client/src/components/layouts/product.js
+++ client/src/components/layouts/product.js
@@ -30,16 +30,15 @@ const Product = () => {
     return (
         <div className="row">
     <div className="col s12 m6">
-      <div className="card white darken-1">
-        <div className="card-content black-text">
+      <div className="card blue-grey darken-1">
+        <div className="card-content white-text">
           <span className="card-title">Card Title</span>
           <p>I am a very simple card. I am good at containing small bits of information.
           I am convenient because I require little markup to use effectively.</p>
         </div>
         <div className="card-action">
-        {/* <Route path='/' exact component={Shop} /> */}
-         <Link to="/product-page" >Buy Now</Link>
-  
+          <Link to="#">This is a link</Link>
+          <Link to="#">This is a link</Link>
         </div>
       </div>
     </div>
