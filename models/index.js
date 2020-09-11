a69973aa0e40e6041b7aa3ca944b0b350b4c6d5c
Corie Lynch <Lynchc17@gmail.com>
2020-09-10 18:11:29 -0700

    setting up package.json for server

--- models/index.js
+++ models/index.js
@@ -1,3 +1,17 @@
-module.exports = {
-    Product: require("./products")
-  };
+
+var Sequelize = require("sequelize");
+var sequelize = require("../config/connection.js");
+
+var Products = sequelize.define("products", {
+  handle: Sequelize.STRING,
+  title: Sequelize.STRING,
+  body: Sequelize.STRING,
+  tags: Sequelize.STRING,
+  price: Sequelize.NUMBER
+});
+
+// Syncs with DB
+Products.sync();
+
+// Makes the Chirp Model available for other files (will also create a table)
+module.exports = Products;
--- server.js
+++ server.js
@@ -1,24 +1,30 @@
-const express = require("express");
 
-const mongoose = require("mongoose");
-const routes = require("./routes");
+const express = require('express');
+
+const path = require('path');
+
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
-// Define middleware here
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/jewelry_db");
+app.use(express.static("client/public"));
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+// Routes
+// =============================================================
+// require("./app/routes/api-routes.js")(app);
+
+// Here we introduce HTML routing to serve different HTML files
+// require("./app/routes/html-routes.js")(app);
+
+
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
