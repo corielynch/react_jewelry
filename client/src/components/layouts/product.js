be512280300333b8d608a06ad28920126533a662
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-11 14:27:03 -0700

    Update product.js
    src waiting till we have DB product info

--- client/src/components/layouts/product.js
+++ client/src/components/layouts/product.js
@@ -1,5 +1,13 @@
 import React from 'react';
-// import src from '*.bmp';
+/* 
+import { Src } from '*.bmp';
+class Button extends Src {
+  render() {
+      }
+}
+export default Button;
+*/
+
 import { Link} from 'react-router-dom'
 
 const products = [
@@ -32,7 +40,7 @@ const Product = () => {
     <div className="col s12 m6">
       <div className="card blue-grey darken-1">
         <div className="card-content white-text">
-          <span className="card-title">Card Title</span>
+          <span className="card-title">Product</span>
           <p>I am a very simple card. I am good at containing small bits of information.
           I am convenient because I require little markup to use effectively.</p>
         </div>
