33d3495361deab6ed635838efa43ff319ab3d3b0
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-12 11:49:02 -0700

    Update App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,14 +12,12 @@ import Account from './pages/Account';
 import Header from './layouts/header/header';
 import ProductPage from './pages/Product-Page';
 
-
 import Nav from './layouts/nav'
 
 class App extends React.Component {
     render(){
         return (
             <div>
-              
                 <BrowserRouter>
                 <div class="Header-Class">
                     <Header />
@@ -39,34 +37,12 @@ class App extends React.Component {
                         <Route path='/FAQ' exact component={Faq} />
                         <Route path='/Account' exact component={Account} />
                         <Route path='/product-page' exact component={ProductPage} />
-                        
                     </div>
                 </BrowserRouter>
             </div>
             )
     }
 }
-// const App =  () => {
-//     return (
-//         <div>
-//           <div></div>
-//             <BrowserRouter>
-//             <div>
-//                 <Nav />
-//             </div>
-//                 <div>
-//                     <Route path='/' exact component={Shop} />
-//                     <Route path='/Home' exact component={Home} />
-//                     <Route path='/About' exact component={About} />
-//                     <Route path='/Press' exact component={Press} />
-//                     <Route path='/Lookbook' exact component={Lookbook} />
-//                     <Route path='/Contact' exact component={Contact} />
-//                     <Route path='/FAQ' exact component={Faq} />
-//                     <Route path='/Account' exact component={Account} />
-//                 </div>
-//             </BrowserRouter>
-//         </div>
-//         )
-// }
+
 
 export default App;
