55531c882768f8d25951d24e8666e3c0afa11c53
Corie Lynch <Lynchc17@gmail.com>
2020-09-11 18:55:26 -0700

    Fixed the server connection

--- models/index.js
+++ models/index.js
@@ -1,17 +1,37 @@
+'use strict';
 
-var Sequelize = require("sequelize");
-var sequelize = require("../config/connection.js");
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-var Products = sequelize.define("products", {
-  handle: Sequelize.STRING,
-  title: Sequelize.STRING,
-  body: Sequelize.STRING,
-  tags: Sequelize.STRING,
-  price: Sequelize.NUMBER
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
-// Syncs with DB
-Products.sync();
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-// Makes the Chirp Model available for other files (will also create a table)
-module.exports = Products;
+module.exports = db;
