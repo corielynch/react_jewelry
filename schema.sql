32778075e43498606468a27b3b9845eb26414c8d
Corie Lynch <Lynchc17@gmail.com>
2020-09-11 17:55:05 -0700

    Schema edits

--- schema.sql
+++ schema.sql
@@ -4,12 +4,15 @@ CREATE DATABASE jewelry_db;
 USE jewelry_db;
 
 CREATE TABLE products (
-id int,
-handle VARCHAR (50),
-title VARCHAR (50),
-body VARCHAR (50),
-tags VARCHAR (50),
-price INTEGER(10)
+id int AUTO_INCREMENT,
+handle VARCHAR (500),
+title VARCHAR (500),
+body VARCHAR (500),
+tags VARCHAR (500),
+price INTEGER(200)
+
+PRIMARY KEY ( `id` ) 
+
 
 );
 
