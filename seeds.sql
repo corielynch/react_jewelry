ec07f94a20086cd57acef60668c5e4c00f57212c
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-12 13:15:14 -0700

    fixed spaces

--- seeds.sql
+++ seeds.sql
@@ -1,6 +1,6 @@
 USE jewelry_db;
 
-INSERT INTO products (handle, title, body, tags, price)
+INSERT INTO products (handle, title, body, tags, price, img_src)
 VALUES ("black-resin-onyx-earrings", "Black Resin & Onyx Earrings", "Exactly what Beyonce wore in her Brown Skin Girl music video!
 Black resin and onyx earrings with white matte Swarovski at the top of the clip-on earrings. Earrings are offered in post earrings as well for pierced ears.", "Earrings", 298);
 
@@ -10,8 +10,8 @@ VALUES ("aries-gold-swarovski-crystal-ring", "Aries Gold and Swarovski Crystal R
 INSERT INTO products (handle, title, body, tags, price, img_src)
 VALUES ("taurus-silver-ring", "Taurus Silver Ring", "Taurus ring in plated silver Sizes: 9 10",	"Rings", 288, "https://cdn.shopify.com/s/files/1/0755/2497/products/ariesGold.png?v=1588459523");
 
-INSERT INTO products (handle, title, body, tags, price)
+INSERT INTO products (handle, title, body, tags, price, img_src)
 VALUES ("aries-gold-swarovski-crystal-ring", "Aries Gold and Swarovski Crystal Ring	Aries", "ring in gold plated and Swarovski crystal","Rings", 288);
 
-INSERT INTO products (handle, title, body, tags, price)
+INSERT INTO products (handle, title, body, tags, price, img_src)
 VALUES ("taurus-silver-ring", "Taurus Silver Ring", "Taurus ring in plated silver Sizes: 9 10",	"Rings", 288);
