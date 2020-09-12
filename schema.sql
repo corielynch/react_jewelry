0ff885bb7c6dd98f4db5b93095cb1e479fef3566
Corie Lynch <Lynchc17@gmail.com>
2020-09-12 11:51:42 -0700

    Updated img src into schema and seeds

--- schema.sql
+++ schema.sql
@@ -3,13 +3,14 @@ DROP DATABASE IF EXISTS jewelry_db;
 CREATE DATABASE jewelry_db;
 USE jewelry_db;
 
-CREATE TABLE products (
+CREATE TABLE product (
 id int AUTO_INCREMENT,
 handle VARCHAR (500),
 title VARCHAR (500),
 body VARCHAR (500),
 tags VARCHAR (500),
-price INTEGER(200)
+price INTEGER(200),
+img_src VARCHAR (500)
 
 PRIMARY KEY ( `id` ) 
 
