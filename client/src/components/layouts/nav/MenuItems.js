ed2bd024756272ae75cf4916d4a12b06cd153503
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-16 09:15:22 -0700

    Create MenuItems.js

---
+++ client/src/components/layouts/nav/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+  {
+    title: 'Consulting',
+    path: '/consulting',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Design',
+    path: '/design',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Development',
+    path: '/development',
+    cName: 'dropdown-link'
+  }
+];
