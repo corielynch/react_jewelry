624f4faaaf85879fc97d962fcb95c5179bf7b16f
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-16 09:44:21 -0700

    Update About.js

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Picks from '../../assets/press.png';
 
 const About = () => {
-    return <div> 
+    return (
+      <div>
+          <hr />
+        <div class="container">
+            <div class="card">
+               <i> <img src={ Picks } /> </i>
+                
              <p>Descended from 2500 years of jewelry addicts, the Persian-American designer, Mona Assemi's namesake brand was founded in Washington DC.  
 Currently residing in New York City, Mona's work combines sculptural clean lines, sumptuous crystals, and elegant metals. She creates a liquid dripping aesthetic frozen in 
     time in the jewelry with an array of formulated acids for her signature ACIDS line. The unusual 14kt plated, silver plated, rose gold plated and gunmetal plated 
@@ -14,6 +21,8 @@ Currently residing in New York City, Mona's work combines sculptural clean lines
  One thing that is seen throughout all her work, is minimalistic lines, the use of repetition and negative space. In addition, Mona likes to always do what's progressive as an emerging designer. You can see her evolve throughout collections aesthetically.  Her biggest and most exciting challenge is to always exceed the lines of what is considered ordinary. She enjoys outside the box accessories. Her work is truly a passion for the art of jewelry. The collections are always fresh, high-end and with edge. In her current collection she has most recently incorporated color with an array of different hues of swarovski crystals. What is said to be her greatest talent is the knack for trend forecasting. 
 Editors are always anticipating Mona's next collection as it is bound to be even better than the last.
           </p>
+            <hr />
     </div>
+  )
 }
 export default About;
