c32bb02c3703124a63e0b4c010d66bd07d7e2b45
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-16 09:35:22 -0700

    Update Faq.js

--- client/src/components/pages/Faq.js
+++ client/src/components/pages/Faq.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 const Faq = () => {
-    return <div>Home Pagee</div>
+    return (
+      <div>
+          <hr />
+        <div class="container">
+            <div class="card">
+               <p> ALL PRODUCTS ARE MADE BY HAND SO PLEASE ALLOW 4-6 WEEKS FOR CREATION + DELIVERY.
+ALL ACIDS COLLECTION STYLES ALSO COME IN NON ACIDS AS WELL IN PLAIN 14 KT PLATED GOLD, ROSE GOLD PLATED, GUNMETAL PLATED, AND SILVER PLATED.
+PLEASE CONTACT MONA@MONAASSEMI.COM WITH YOUR ORDER NUMBER AND YOUR CHOICE OF METAL.
+JEWELRY, WILL BE THE CLOSEST VARIATION TO THE PIECES PICTURED WHEN SHIPPED. />
+                              
+            </div>
+        </div>
+        <hr />
+    </div>
+    )
 }
 export default Faq;
