1b63321471cb4059ff363862544007e393f0ff19
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-16 09:32:57 -0700

    Update Press.js

--- client/src/components/pages/Press.js
+++ client/src/components/pages/Press.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import Pic from '../../assets/press.png';
 
 const Press = () => {
-    return <div>Home Pagee</div>
+    return (
+      <div>
+          <hr />
+        <div class="container">
+            <div class="card">
+               <i> <img src={ Pic } />
+                //press images//
+                
+            </div>
+        </div>
+        <hr />
+    </div>
+    )
 }
 export default Press;
