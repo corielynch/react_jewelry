0b28a5a12700cfa68fad4c2fa791c62c2bd0e36f
Batman <bgmyhan@gmail.com>
2020-09-15 19:20:33 -0700

    controller that gets product data from sql database

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -4,68 +4,10 @@ const { QueryTypes } = require('sequelize');
 const { sequelize } = require('../models/index.js');
 // get all products from database
 
-// exports.productsAll = function(res) {
-//   db.products.findAll().then(function(result) {
-//     console.log(result)
-//     return res.json(result);
-   
-//     })
-//   };
+  exports.productsAll = function(req, res) {
 
-exports.productsAll = function(req, res) {
-
-  db.products.findAll({}).then((result) => {
-   console.log("products:", result)
-   return res.json(result);
- })
+  db.products.findAll({}).then((results) => {
+   console.log(results.products.id)
+   return res.json(results);
+  })
 };
-
-
-// exports.productsAll = function(req, res) {
-//   db.product.findAll().then((result) => {
-//    return res.json(result);
-//    console.log(req.body)
-//  })
-// };
-
-
-// exports.productsAll = function(res) {
-//   db.product.findAll().then((result) => {
-
-//    console.log(result)
-//    res.json(result);
-    
-//  })
-// };
-
-// exports.productsAll = function(res) {
-//    db.product.findAll().then((result) => {
-//     try {
-//     console.log(result)
-//     return res.json(result);
-//       } catch(err) {
-//       console.log(err);
-//       }
-//   })
-// };
-
-
-  // db.User.findAll({
-    //   where: {username: req.body.username}
-    // }).then(function(users) {
-    //   if (users.length > 0) {
-    //     res.json({
-    //       duplicateUser: true
-    //     });
-    //   //At some point, make sure that only one user can be associated with an email.
-    //   } else {
-    //     db.User.create({
-    //       username: req.body.username,
-    //       email: req.body.email,
-    //       password: req.body.password
-    //     }).then(function() {
-    //       res.send({redirect: '/'});
-    //     }).catch(function(err) {
-    //       res.json(err);
-    //     });
-    //   }
