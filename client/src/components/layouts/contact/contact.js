3cfe5588fd34d46852b934fad6961354981ebe6d
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-16 13:08:26 -0700

    Update contact.js
    test

--- client/src/components/layouts/contact/contact.js
+++ client/src/components/layouts/contact/contact.js
@@ -1,70 +1,32 @@
-import React from 'react';
-import axios from 'axios';
-import './contact.css'
-
-class Contact extends React.Component{
-  constructor(props) {
-	super(props);
-	this.state = {
-  	name: '',
-  	email: '',
-  	message: ''
-	}
-  }
-
-  handleSubmit(e){
-    e.preventDefault();
-    axios({
-      method: "POST", 
-      url:"http://localhost:3000/send", 
-      data:  this.state
-    }).then((response)=>{
-      if (response.data.status === 'success'){
-        alert("Message Sent."); 
-        this.resetForm()
-      }else if(response.data.status === 'fail'){
-        alert("Message failed to send.")
-      }
-    })
-  }
-
-  resetForm(){
-     this.setState({name: '', email: '', message: ''})
-  }
-  
-  render() {
-	return(
-        <div className="Contact">
-            <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-                    <div className="form-group">
-                        <label htmlFor="name">Name</label>
-                        <input type="text" className="form-control" id="name" value={this.state.name} onChange={this.onNameChange.bind(this)} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="message">Message</label>
-                        <textarea className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
-                    </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+  <section id="contact" class="section section-contact scrollspy">
+    <div class="container">
+      <div class="row">
+        <div class="col s12 m6">
+          <div class="card-panel yellow darken-2 white-text center">
+            <i class="material-icons">email</i>
+            <h5>Email</h5>
+            <p>info@monassemi.com<p>
+          </div>
+          <div class="card-panel black amber-text center">
+            <i class="material-icons">location_on</i>
+            <h5>Location</h5>
+            <p>13181 Crossroads Parkway, Suite 580</p>
+            <p>City of Industry, CA 91746</p>
+          </div>
+          <div class="card-panel  white blue-text center">
+            <i class="material-icons">phone</i>
+            <h5>Phone</h5>
+            <p>Toll Free: 888-597-5556</p>
+            <p><hr>Direct: 562-326-5743</p>
+            <p>Fax---: 562-463-5726</p>
+          </div>
         </div>
-	);
-  }
-
-  onNameChange(event) {
-	this.setState({name: event.target.value})
-  }
-
-  onEmailChange(event) {
-	this.setState({email: event.target.value})
-  }
-
-  onMessageChange(event) {
-	this.setState({message: event.target.value})
-  }
-}
-
-export default Contact;
+          <div><div class="col s12 m6">
+            <iframe src="https://www.powr.io/form-builder/u/f45433e8_1588974109#platform=iframe" style="width:100%;" height="750px" frameborder="0"></iframe>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </section>
