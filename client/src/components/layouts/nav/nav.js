801c0fd26492b1fb73a0e14445286713db672804
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-16 09:05:24 -0700

    Update nav.js

--- client/src/components/layouts/nav/nav.js
+++ client/src/components/layouts/nav/nav.js
@@ -1,42 +1,103 @@
-import React, { useState } from 'react'
-import { Button } from './Button'
-import { Link } from 'react-router-dom'
-import './style.css'
-import Dropdown from './Dropdown'
+import React, { useState } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import Dropdown from './Dropdown';
 
-class Nav extends Component {
-  render(){
-    return(
-      <div>
-    
-  <nav>
-    <div className="nav-wrapper">
-      <a href="/home" className="brand-logo">Logo</a>
-      <a href="/" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-      <ul className="right hide-on-med-and-down">
-        <li><a href="/Home" >HOME</a></li>
-        <li><a href="/Shop" >SHOP</a></li>
-        <li><a href="/About"  >ABOUT</a></li>
-        <li><a href="/Press"  >PRESS</a></li>
-        <li><a href="/Lookbook"  >LOOKBOOK</a></li>
-        <li><a href="/Contact"  >CONTACT</a></li>
-        <li><a href="/FAQ"  >FAQ</a></li>
-        <li><a href="mobile.html" >Mobile</a></li>
-      </ul>
-    </div>
-  </nav>
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
 
-  <ul className="sidenav" id="mobile-demo">
-    <li><a href="sass.html" className="black">Sass</a></li>
-    <li><a href="badges.html" className="black">Components</a></li>
-    <li><a href="collapsible.html" className="black">Javascript</a></li>
-    <li><a href="mobile.html" >Mobile</a></li>
-  </ul>
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-      </div>
-    )
-  }
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
+
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
+
+  return (
+    <>
+      <nav className='navbar'>
+        <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+          MONA ASSEMI
+          <i class='fab fa-firstdraft' />
+        </Link>
+        <div className='menu-icon' onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li
+            className='nav-item'
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <Link
+              to='/faq'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Faq <i className='fas fa-caret-down' />
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/shop'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Shop
+            </Link>
+          </li>
+    <li className='nav-item'>
+            <Link
+              to='/lookbook'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Lookbook
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/contact-us'
+              className='nav-links'
+              onClick={closeMobileMenu}
+            >
+              Contact Us
+            </Link>
+          </li>
+          <li>
+            <Link
+              to='/account'
+              className='nav-links-mobile'
+              onClick={closeMobileMenu}
+            >
+              Account
+            </Link>
+          </li>
+        </ul>
+        <Button />
+      </nav>
+    </>
+  );
 }
-  
-  export default Nav;
- 
+
+export default Navbar;
