a521dc36a3076c2e18709db72f7110dd805c1f6c
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-16 09:38:29 -0700

    Update Lookbook.js

--- client/src/components/pages/Lookbook.js
+++ client/src/components/pages/Lookbook.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import Pick from '../../assets/look.png';
 
 const Lookbook = () => {
-    return <div>Home Pagee</div>
+    return (
+      <div>
+          <hr />
+        <div class="container">
+            <div class="card">
+               <i> <img src={ Pick } />
+                             
+            </div>
+        </div>
+        <hr />
+    </div>
+    )
 }
 export default Lookbook;
