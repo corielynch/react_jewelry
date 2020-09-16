3ac1be111313394b2f8b02ec87131f1bee924edf
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-16 09:30:13 -0700

    Update Home.js

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import '../../App.css'; 
 
 const HomePage = () => {
-    return <div> MonaAssemi Jew </div>
+    return <div> MonaAssemi Jewelry </div>
 }
 export default HomePage;
