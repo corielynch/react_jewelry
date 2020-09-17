28a2d4bb29b8a70eac20cd7bd35c8444293d94df
Corie Lynch <Lynchc17@gmail.com>
2020-09-16 22:40:09 -0700

    CRUD routes and controller

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -1,13 +1,44 @@
 
-var db = require('../models/index.js');
-const { QueryTypes } = require('sequelize');
-const { sequelize } = require('../models/index.js');
+const db = require('../models/index.js');
 // get all products from database
 
-  exports.productsAll = function(req, res) {
+module.exports = {
+  productsAll: function (req, res) {
 
-  db.products.findAll({}).then((results) => {
-   console.log(results)
-   return res.json(results);
-  })
-};
+    db.products.findAll({}).then((results) => {
+      return res.json(results);
+    })
+  },
+  createProduct: function(req,res){
+    console.log(req.body)
+    db.products.create(req.body).then(results => {
+      console.log(results)
+      res.json(results)
+    })
+  }, 
+  updateProduct: function(req,res){
+    const id = req.params.id
+    console.log(id,req.body)
+    db.products.update(
+      req.body,
+      {
+      where: {
+        id:id
+      }
+    }).then(results => {
+      res.json(results)
+    })
+  },
+  deleteProduct: function(req,res){
+    const id = req.params.id
+    db.products.destroy({
+      where: {
+        id:id
+      }
+    }).then(results => {
+      res.json(results)
+    })
+  }
+
+}
+
--- routes/products.js
+++ routes/products.js
@@ -4,67 +4,11 @@ var products_controller = require('../controllers/products_controller');
 
 router.get('/api/search-products', products_controller.productsAll);
 
+router.post('/api/search-products', products_controller.createProduct)
+
+router.put('/api/search-products/:id', products_controller.updateProduct)
+
+router.delete('/api/search-products/:id', products_controller.deleteProduct)
 
 module.exports = router;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// module.exports = function(app) {
-//     // Search for Specific Products (or all products) then provides JSON
-//     // app.get("/api/:products", function(req, res) {
-//     //     console.log(req, res)
-//     //   if (req.params.products) {
-//     //     // Display the JSON for ONLY that product.
-//     //     // (Note how we're using the ORM here to run our searches)
-//     //     Products.findOne({
-//     //       where: {
-//     //         routeName: req.params.products
-//     //       }
-//     //     }).then(function(result) {
-//     //       return res.json(result);
-          
-//     //     });
-//     //   } else {
-//     //     Products.findAll().then(function(result) {
-//     //       return res.json(result);
-//     //     });
-//     //   }
-
-
-//     async function asyncFunc() {
-//         let data;
-//         // fetch data from a url endpoint
-//         axios.get("/api/products")
-//           .then((result) => {
-//             data = result
-//           });
-//         return data;
-//         console.log(data)
-//       }
-//     // });
-// }
