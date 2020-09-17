c4731e951f2a4621ae3816936dce9b2bf13faf3f
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-17 00:22:53 -0700

    cart=orderpage

--- client/src/components/pages/Order-Page.js
+++ client/src/components/pages/Order-Page.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const OrderPage = () => {
-    return <div>Customer input informtion and order Summary 
-        with buy button functionality. Most likely will be 
-        rendered from our shopify token once we get that code from shopify.com</div>
-}
+
+    const OrderPage = () => {
+        return (
+          <div>
+              <hr />
+            <div class="container">
+                <div class="image">
+                    <img src={ look } alt= 'photos of jewelry' />
+                </div>
+            </div>
+            <hr />
+        </div>
+        )
+    }
 export default OrderPage;
 
 
