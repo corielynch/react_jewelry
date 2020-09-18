4a5dd97b5a319b518ccc49b928d182a5929bb806
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-18 09:40:43 -0700

    Update product.js
    omg just found this great website. it spaces the cards. the classname is card deck! its like you keep hitting your head on a brick wall and then suddenly the wall is go and things actually work!!!

--- client/src/components/layouts/product/product.js
+++ client/src/components/layouts/product/product.js
@@ -6,31 +6,78 @@ import './product.css'
 
 const Product = ({product}) => {
   
-    return (
-      <div>
-        <div className="container">
-         <div className="wrapper"> </div>
-            <span className="price">{product.price}</span>
-                <div className="image">
-                  <img src={product.img_src} alt={product.handle}></img>
-                </div>
-                <div className="card-content">
-                  <div className="wrapper">
-                    <div className="title">{product.title}</div>  
-                      <p>{product.title}</p> 
-                        <span className="price">{product.price}</span> 
-                          <div className="content tag">
-                            <div className="name tag-name">{product.tag}</div>
-                              <div className="size-value">
-                                <span className="category">{product.tag}</span>
-                              </div>
-                            </div>
-                          </div>
-                      </div>
-                    </div>
-                  </div>
+//     return (
+//       <div>
+//         <div className="container">
+//          <div className="wrapper"> </div>
+//             <span className="price">{product.price}</span>
+//                 <div className="image">
+//                   <img src={product.img_src} alt={product.handle}></img>
+//                 </div>
+//                 <div className="card-content">
+//                   <div className="wrapper">
+//                     <div className="title">{product.title}</div>  
+//                       <p>{product.title}</p> 
+//                         <span className="price">{product.price}</span> 
+//                           <div className="content tag">
+//                             <div className="name tag-name">{product.tag}</div>
+//                               <div className="size-value">
+//                                 <span className="category">{product.tag}</span>
+//                               </div>
+//                             </div>
+//                           </div>
+//                       </div>
+//                     </div>
+//                   </div>
+  <CardDeck>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" fluid />
+    <Card.Body>
+      <Card.Title>{product.name}</Card.Title>
+      <Card.Text></Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">product.price</small>
+    </Card.Footer>
+  </Card>
+  
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" fluid />
+    <Card.Body>
+      <Card.Title>{product.name}</Card.Title>
+      <Card.Text>
+        {product.description}
+      </Card.Text>
+     <Button variant="primary">Buy Now</Button>
+    </Card.Body>
+  </Card>
+  
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" fluid />
+    <Card.Body>
+      <Card.Title>{product.name}</Card.Title>
+      <Card.Description>
+         </Card.Description>
+    </Card.Body>
+    <Card.Footer>
+       <Form.Group as={Row}>
+    <Col sm={{ span: 10, offset: 2 }}>
+      <Button type="submit">Buy Now</Button>
+    </Col>
+  </Form.Group>
+//         <Button animated='vertical'>
+//       <Button.Content hidden>Shop</Button.Content>
+//       <Button.Content visible>
+//         <Icon name='shop' />
+//       </Button.Content>
+    </Card.Footer>
+  </Card>
+</CardDeck>
+
     );
 }
 
 export default Product;
+
+
 
