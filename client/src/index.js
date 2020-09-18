4fb3cf68efcabbe94e9f3d700d88948a51cc02ee
Wilson <wilsonhorrell@git.training>
2020-09-18 17:45:38 -0500

    again

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,4 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 
 // Take the react component and print to screen
-ReactDOM.render(
-    // for the footer style, please don't delete from 10-15 all together.
-    <>
-        <App />
-    </>
-    ,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector('#root'))
