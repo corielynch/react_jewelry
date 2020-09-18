8b33763dfff586d86a1ed60e94c0d7072ce3a954
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-18 09:52:51 -0700

    Update NavList.js

--- client/src/components/layouts/nav/NavList.js
+++ client/src/components/layouts/nav/NavList.js
@@ -1,3 +1,10 @@
+<Breadcrumb>
+  <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+  <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+    Library
+  </Breadcrumb.Item>
+  <Breadcrumb.Item active>Data</Breadcrumb.Item>
+</Breadcrumb>
 export const NavList = [
     {
         title: 'Home',
