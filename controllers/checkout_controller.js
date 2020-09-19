81097b653e8a737edd784cc2e6e1aa64901ef7d0
Wilson <wilsonhorrell@git.training>
2020-09-18 19:07:45 -0500

    Again

--- controllers/checkout_controller.js
+++ controllers/checkout_controller.js
@@ -1,11 +1,11 @@
 const db = require("../models/index.js");
 
 module.exports = {
-    createCheckout: function(req,res){
+    createCheckout: function(req, res) {
         db.checkout.create(req.body).then(results => {
             console.log(results)
             res.json(results)
-            .catch(err => res.status(422).json(err));
-        })
+
+        }).catch(err => res.status(422).json(err));
     }
 }
--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -1,45 +1,42 @@
-
 const db = require('../models/index.js');
 // get all products from database
 
 module.exports = {
-  productsAll: function (req, res) {
+    productsAll: function(req, res) {
 
-    db.products.findAll({}).then((results) => {
-      return res.json(results)
-      .catch(err => res.status(422).json(err));
-    })
-  },
-  createProduct: function(req,res){
-    db.products.create(req.body).then(results => {
-      res.json(results)
-      .catch(err => res.status(422).json(err));
-    })
-  }, 
-  updateProduct: function(req,res){
-    const id = req.params.id
-    db.products.update(
-      req.body,
-      {
-      where: {
-        id:id
-      }
-    }).then(results => {
-      res.json(results)
-      .catch(err => res.status(422).json(err));
-    })
-  },
-  deleteProduct: function(req,res){
-    const id = req.params.id
-    db.products.destroy({
-      where: {
-        id:id
-      }
-    }).then(results => {
-      res.json(results)
-      .catch(err => res.status(422).json(err));
-    })
-  }
+        db.products.findAll({}).then((results) => {
+            return res.json(results)
+
+        }).catch(err => res.status(422).json(err));
+    },
+    createProduct: function(req, res) {
+        db.products.create(req.body).then(results => {
+            res.json(results)
+
+        }).catch(err => res.status(422).json(err));
+    },
+    updateProduct: function(req, res) {
+        const id = req.params.id
+        db.products.update(
+            req.body, {
+                where: {
+                    id: id
+                }
+            }).then(results => {
+            res.json(results)
+
+        }).catch(err => res.status(422).json(err));
+    },
+    deleteProduct: function(req, res) {
+        const id = req.params.id
+        db.products.destroy({
+            where: {
+                id: id
+            }
+        }).then(results => {
+            res.json(results)
+
+        }).catch(err => res.status(422).json(err));
+    }
 
 }
-
