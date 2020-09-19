f81e4976648aca8d5cc2e2f6a67ad3cbd46dbc87
Corie Lynch <Lynchc17@gmail.com>
2020-09-19 09:20:19 -0700

    Uncommented things

--- client/src/components/layouts/product/product.js
+++ client/src/components/layouts/product/product.js
@@ -1,10 +1,9 @@
-// import React from 'react';
+import React from 'react';
 // import {
 //   Card, CardImg, CardText, CardBody,
 //   CardTitle, CardSubtitle, Button
 // } from 'reactstrap';
 
-<<<<<<< HEAD
 // Product component renders a single product card from the productlist component //
 const Product = ({product}) => {
   
@@ -35,22 +34,3 @@ const Product = ({product}) => {
 }
 export default Product;
 
-=======
-// const Cart = () => {
-//   return (
-//     <div>
-//       <Card>
-//         <CardImg top width="100%" src="'../../assets/mona.png" alt="Card image cap" />
-//         <CardBody>
-//           <CardTitle>Card title</CardTitle>
-//           <CardSubtitle>Card subtitle</CardSubtitle>
-//           <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-//           <Button variant="warning">Buy Now</Button>
-//         </CardBody>
-//       </Card>
-//     </div>
-//   );
-// };
-
-// export default Cart;
->>>>>>> css
