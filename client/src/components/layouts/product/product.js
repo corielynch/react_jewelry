0ea2602de37bec6886e3b293987a35599579350d
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-21 09:49:16 -0700

    Update product.js

--- client/src/components/layouts/product/product.js
+++ client/src/components/layouts/product/product.js
@@ -1,23 +1,36 @@
-// import React from 'react';
-// import {
-//   Card, CardImg, CardText, CardBody,
-//   CardTitle, CardSubtitle, Button
-// } from 'reactstrap';
+import React from 'react';
+import './product.css'
+// import ProductsList from './productsList';
 
-// const Cart = () => {
-//   return (
-//     <div>
-//       <Card>
-//         <CardImg top width="100%" src="'../../assets/mona.png" alt="Card image cap" />
-//         <CardBody>
-//           <CardTitle>Card title</CardTitle>
-//           <CardSubtitle>Card subtitle</CardSubtitle>
-//           <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-//           <Button variant="warning">Buy Now</Button>
-//         </CardBody>
-//       </Card>
-//     </div>
-//   );
-// };
 
-// export default Cart;
+
+const Product = ({product}) => {
+    const id= document.location + product.id + product.handle
+    return (
+      <div>
+        <div className="container">
+         <div className="wrapper"> </div>
+            <span className="price">{product.price}</span>
+                <div className="image">
+                  <img src={product.img_src} alt={product.handle}></img>
+                </div>
+                <div className="card-content">
+                  <div className="wrapper">
+                    <div className="title">{product.title}</div>  
+                      <p>{product.title}</p> 
+                        <span className="price">{product.price}</span> 
+                          <div className="content tag">
+                            <div className="name tag-name">{product.tag}</div>
+                              <div className="size-value">
+                                <span className="category">{product.tag}</span>
+                              </div>
+                              <button><a href={id}>Buy Now</a></button>
+                            </div>
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+    );
+}
+
+export default Product;
