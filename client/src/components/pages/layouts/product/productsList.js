f7083551c7c97e280dd4b0d7fbf490ad79b59acb
Batman <bgmyhan@gmail.com>
2020-09-25 23:50:00 -0700

    Reworked productList to display a list of all the products to the shop page. Still unable to get old CSS product card to show up like before

--- client/src/components/pages/layouts/product/productsList.js
+++ client/src/components/pages/layouts/product/productsList.js
@@ -3,17 +3,49 @@ import { connect } from 'react-redux';
 import { fetchProducts } from '../../../../actions'
 import Product from './product'
 import getProducts from '../../../../apis/productsDB'
+import './product.css'
 
 
 class ProductsList extends React.Component {
    componentDidMount(){
       this.props.fetchProducts()
    }
-   render() {
-      console.log(this.props.products)
-      return <div>Hello</div>
+
+   renderList(){
+      return this.props.products.map( product => {
+         return (
+         <div className="container" key={product.id}>
+            <div className="wrapper"> 
+               <span className="price">{product.price}</span>
+               <div className="image">
+                  <img src={product.img_src} alt={product.handle}></img>
+               </div>
+               <div className="card-content">
+                  <div className="wrapper">
+                     <div className="title">{product.title}</div>  
+                     <p>{product.title}</p> 
+                     <span className="price">{product.price}</span> 
+                        <div className="content tag">
+                           <div className="name tag-name">{product.tag}</div>
+                           <div className="size-value">
+                           <span className="category">{product.tag}</span>
+                           </div>
+                        </div>
+                  </div>
+               </div>
+            </div>
+         </div>   
+           
+         )
+      })
    }
+
+   render() {
+      return <div>{this.renderList()}</div>
+   }
+
 }
+
 
 const mapStateToProps = (state) => {
    return { products: state.products}
