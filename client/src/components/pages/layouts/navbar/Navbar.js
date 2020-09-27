2e6a5cde092f2ecfe134eb6df57e351431dc73a9
Kathleen Yoannon <k.yoannon@gmail.com>
2020-09-26 19:01:54 -0700

    Update Navbar.js

--- client/src/components/pages/layouts/navbar/Navbar.js
+++ client/src/components/pages/layouts/navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Nav} from "react-bootstrap";
 import { NavbarItems } from "./NavbarItems"
 import './Navbar.css'
 
@@ -11,26 +12,15 @@ class Navbar extends Component {
 
     render() {
         return(
-            <nav className="NavbarItem">
-                <div className="menu-icon" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
-                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-                    {NavbarItems.map((item, index) => {
+            <Nav className="justify-content-center" activeKey="/home">
+                {NavbarItems.map((item, index) => {
                         return (
-                            <li key={index}>
-                                <a className={item.cName} href={item.url}>
-                                {item.title}
-                                </a>
-                            </li>
+                            <Nav.Item>
+                                <Nav.Link href={item.url}>{item.title}</Nav.Link>
+                            </Nav.Item>
                         )
                     })}
-                </ul>
-        <div className="cart hide"><i className="fas fa-shopping-cart"></i>0</div>
-            </nav>
-        )
-    }
-}
-export default Navbar;
+            </Nav>
 
+export default Navbar;
 
