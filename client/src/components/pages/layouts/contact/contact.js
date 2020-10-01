f12ca5e56afd3d187b7f4481b0b349ce6068974d
Kathleen Yoannon <k.yoannon@gmail.com>
2020-10-01 14:34:12 -0700

    Update contact.js

--- client/src/components/pages/layouts/contact/contact.js
+++ client/src/components/pages/layouts/contact/contact.js
@@ -1,28 +1,58 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle } from 'reactstrap';
+import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-const Cart = () => {
+const Cart = (props) => {
   return (
-    <div>
-      <Card>
-      <CardTitle>Contact</CardTitle>
-      <CardImg top width="50%" src="https://cdn.shopify.com/s/files/1/0755/2497/files/pro-02-Sa.jpg?9755218888888724393" alt="Card image cap" />
-        <CardImg top width="50%" src="https://cdn.shopify.com/s/files/1/0755/2497/files/pro-03-Sa.jpg?17436661442076790054" alt="Card image cap" />
-        <CardBody>
-          <CardSubtitle>Love hearing from you</CardSubtitle>
-          <CardText>Mona likes to always do what's progressive as an emerging designer.
-             You can see her evolve throughout collections aesthetically.  Her biggest and most exciting 
-             challenge is to always exceed the lines of what is considered ordinary. She enjoys outside
-              the box accessories. Her work is truly a passion for the art of jewelry. The collections are always 
-              fresh, high-end and with edge. In her current collection she has most recently incorporated color 
-              with an array of different hues of swarovski crystals. What is said to be her greatest talent is the
-               knack for trend forecasting. Editors are always 
-            anticipating Mona's next collection as it is bound to be even better than the last. </CardText>
-        </CardBody>
-      </Card>
-    </div>
+    <Form>
+      <Row form>
+        <Col md={6}>
+          <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input type="email" name="email" id="exampleEmail" placeholder="Type email here" />
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
+            <Label for="examplePassword">Password</Label>
+            <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
+          </FormGroup>
+        </Col>
+      </Row>
+      <FormGroup>
+        <Label for="exampleAddress">Address</Label>
+        <Input type="text" name="address" id="exampleAddress" placeholder="1234 Main St"/>
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleAddress2">Address 2</Label>
+        <Input type="text" name="address2" id="exampleAddress2" placeholder="Apartment, studio, or floor"/>
+      </FormGroup>
+      <Row form>
+        <Col md={6}>
+          <FormGroup>
+            <Label for="exampleCity">City</Label>
+            <Input type="text" name="city" id="exampleCity"/>
+          </FormGroup>
+        </Col>
+        <Col md={4}>
+          <FormGroup>
+            <Label for="exampleState">State</Label>
+            <Input type="text" name="state" id="exampleState"/>
+          </FormGroup>
+        </Col>
+        <Col md={2}>
+          <FormGroup>
+            <Label for="exampleZip">Zip</Label>
+            <Input type="text" name="zip" id="exampleZip"/>
+          </FormGroup>  
+        </Col>
+      </Row>
+      <FormGroup check>
+        <Input type="checkbox" name="check" id="exampleCheck"/>
+        <Label for="exampleCheck" check>Remeber me</Label>
+      </FormGroup>
+      <Button>Sign up</Button>
+    </Form>
   );
-};
+}
 
 export default Cart;
