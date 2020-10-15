5eb42da1e0a22c8f3b457f2106a381234c425fc6
Corie Lynch <Lynchc17@gmail.com>
2020-10-14 19:44:14 -0700

    Trying to get navbar to show

--- client/src/components/pages/layouts/navbar/Navbar.js
+++ client/src/components/pages/layouts/navbar/Navbar.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import {Nav} from "react-bootstrap";
 import { NavbarItems } from "./NavbarItems"
-import './Navbar.css'
+import "./Navbar.css"
 
-class Navbar extends Component {
+const Nav = () => {
     state = { clicked: false }
-
     handleClick = () => {
         this.setState({ clicked: !this.state.clicked })
     }
-
     render() {
         return(
-            <Nav className="justify-content-center" activeKey="/home">
-                {NavbarItems.map((item, index) => {
+            <nav className="NavbarItem">
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    {NavbarItems.map((item, index) => {
                         return (
-                            <Nav.Item>
-                                <Nav.Link href={item.url}>{item.title}</Nav.Link>
-                            </Nav.Item>
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                {item.title}
+                                </a>
+                            </li>
                         )
                     })}
-            </Nav>
-
-export default Navbar;
-
+                </ul>
+        <div className="cart hide"><i className="fas fa-shopping-cart"></i>0</div>
+            </nav>
+        )
+    }
+}
+export default Nav;
