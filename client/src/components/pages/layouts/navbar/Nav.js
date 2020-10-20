cf0f28337fa73700caeb06e36b3b359602495060
Corie Lynch <Lynchc17@gmail.com>
2020-10-20 17:59:30 -0400

    Made Nav a class component

--- client/src/components/pages/layouts/navbar/Nav.js
+++ client/src/components/pages/layouts/navbar/Nav.js
@@ -1,33 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavbarItems } from "./NavbarItems"
 import "./Navbar.css"
 
-        const Nav = () => {
-            state = { clicked: false }
-            handleClick = () => {
-                this.setState({ clicked: !this.state.clicked })
-            }
-            render = () => {
-                return(
-                    <nav className="NavbarItem">
-                        <div className="menu-icon" onClick={this.handleClick}>
-                            <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-                        </div>
-                        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-                            {NavbarItems.map((item, index) => {
-                                return (
-                                    <li key={index}>
-                                        <a className={item.cName} href={item.url}>
+
+class Navigation extends React.Component {
+    state = { clicked: false }
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
+        render () {
+            return (
+                <nav className="NavbarItem">
+                    <div className="menu-icon" onClick={this.handleClick}>
+                        <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                    </div>
+                    <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                        {NavbarItems.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <a className={item.cName} href={item.url}>
                                         {item.title}
-                                        </a>
-                                    </li>
-                                )
-                            })}
-                        </ul>
-                <div className="cart hide"><i className="fas fa-shopping-cart"></i>0</div>
-                    </nav>
-                )
-            }
+                                    </a>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    <div className="cart hide"><i className="fas fa-shopping-cart"></i>0</div>
+                </nav>
+            )
         }
-    
-export default Nav;
+    }
+
+
+export default Navigation;
